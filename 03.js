356a530af98b46f223bce6b9236c3bfa1220e854
PRAVESH <77372438+pravesh0711@users.noreply.github.com>
2024-06-15 11:08:07 +0000

    4

--- 03.js
+++ 03.js
@@ -2,7 +2,7 @@
 let score = true
 // console.log(typeof(score))
 
-let value = Number(score)
+// let value = Number(score)
 
 // console.log(typeof value, value)
 
@@ -22,7 +22,51 @@ let boolainLog = Boolean(isLoggedin);
 // console.log(boolainLog)
 
 let sum = 34
-console.log(typeof sum)
-let newSum = String(sum);
-console.log(typeof newSum)
-console.log(newSum)
+// console.log(typeof sum)
+// let newSum = String(sum);
+// console.log(typeof newSum)
+// console.log(newSum)
+
+
+// operation 
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(3+3)
+// console.log(3-3)
+// console.log(3*3)
+// console.log(3/3)
+// console.log(3%3)
+// console.log(3**3)
+
+
+let str1 = "hello"
+let str2 = "pravesh"
+
+let str3 = str1+str2
+
+
+// console.log(str3)
+
+// console.log("1" +2)
+// console.log(1+"2")
+// console.log("1" + 2+ 2)
+// console.log(1+"2" + 2)
+// console.log(1+2+"2")
+
+// let num1,num2,num3
+
+// num1=num2=num3=num3 = 4
+// console.log()
+
+
+let count  = 100
+let y = count++
+console.log(y)
+++count
+console.log(count)
+
+
