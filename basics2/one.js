3798f1a6a0dc176d6ac8062864f280373b2e8e82
PRAVESH <77372438+pravesh0711@users.noreply.github.com>
2024-06-16 09:57:26 +0000

    spice and slice

---
+++ basics2/one.js
@@ -0,0 +1,25 @@
+const myarr =  [ 0,1,2,3,4,5]
+
+// console.log(myarr)
+// myarr.push(4);
+// console.log(myarr)
+// const newArr = myarr
+// newArr.push(5);
+// console.log(newArr);
+// console.log(myarr)
+
+// myarr.shift()
+// console.log("arr nor ",myarr)
+// const nearr = myarr.join()
+// console.log(nearr);
+
+
+// slice and splice
+// const my1 = myarr.slice(1,3);
+// console.log("arr after slice",myarr)
+// console.log("slice arr ",my1)
+// const my2 = myarr.splice(1,3)
+// console.log("arr after spice ",myarr)
+// console.log("spice arr ", my2)
+
+
