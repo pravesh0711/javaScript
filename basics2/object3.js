78136709da9154c810b3af914b63a159f756cc16
PRAVESH <77372438+pravesh0711@users.noreply.github.com>
2024-06-17 07:23:59 +0000

    object3

---
+++ basics2/object3.js
@@ -0,0 +1,16 @@
+const course = {
+    "name":"jai ho",
+    price:965,
+    teacher:"kamal"
+}
+
+
+const {teacher:t} = course;
+console.log(t);
+console.log(course["name"]);
+
+[
+    {},
+    {},
+    {}
+]
